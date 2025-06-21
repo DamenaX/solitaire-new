03721d0baf3e1a9276d3f1148cf80c9b35d65e9d
Daniel Alemu <danielalemu011@gmail.com>
2025-06-21 14:31:33 +0300

    separate sample drag drop implementation from main script

---
+++ drag-drop-implementation.js
@@ -0,0 +1,27 @@
+// {
+// const cards = document.getElementsByClassName("card");
+// const places = document.getElementsByClassName("place");
+
+// //Save the ID of the dragged card when dragging starts on each card
+// for (card of cards) { 
+//     card.addEventListener("dragstart", (e) => {
+//         e.dataTransfer.setData("text/plain", e.target.id);
+//     })
+// }
+
+
+// // Prevent default behaviour when drag is over and append the dragged card when dropped.
+// for (place of places) {
+//     place.addEventListener("dragover", (e) => {
+//         e.preventDefault();
+//     })
+
+//     place.addEventListener("drop", (e) => {
+//         console.log("there");
+//         let draggedCard = e.dataTransfer.getData("text/plain");
+//         if (e.target.classList.contains("place")) {
+//             e.target.append(document.getElementById(draggedCard));
+//         }
+//     })
+// }
+// }
