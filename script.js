663f51236480e397ae59ed6846915df0315a3a31
Daniel Alemu <danielalemu011@gmail.com>
2025-06-25 14:08:25 +0300

    Add Solitaire Rules to the move function

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let deckArr = [];
 let activePlaceArr = [];
 let foundationPlaceArr = [];
+let hasBeenDealt;
 
 const cards = document.getElementsByClassName("card");
 const places = document.getElementsByClassName("place");
@@ -11,28 +12,23 @@ const deckPile = document.getElementById("deck-pile");
 for (let i = 0; i < 7; i++) activePlaceArr[i] = [];
 for (let i = 0; i < 4; i++) foundationPlaceArr[i] = [];
 
-for (place of places) { // initiate the places to accept cards
+for (card of cards) { // Capture the ID of the dragged card
+    card.addEventListener("dragstart", (e) => {
+        e.dataTransfer.setData("text/plain", e.target.id);
+    })
+}
+
+for (place of places) { // setup the places to accept cards
     place.addEventListener("dragover", (e) => {
         e.preventDefault();
     })
     place.addEventListener("drop", (e) => {
-        let topDisplacement = 0;
-
-
-        let draggedCard = document.getElementById(e.dataTransfer.getData("text/plain"));
-        if (e.target.classList.contains("place")) {
-            e.target.append(draggedCard);
-            topDisplacement = 0;
-        } else if (e.target.classList.contains("card")) {
-            e.target.parentElement.append(draggedCard);
-            topDisplacement = 20 * (e.target.parentElement.children.length - 1);
-            console.log(topDisplacement);
-            draggedCard.style.top =  `${topDisplacement}px`;
-        }
-        draggedCard.style.top =  `${topDisplacement}px`;
+        let draggedCardID = e.dataTransfer.getData("text/plain");
+        let target = e.target;
+        console.log(target);
+        move(draggedCardID, target);
     })
 }
-
 
 // The loop initiates the deck (fills it with the 52 standard cards.)
 function initiateDeck() {
@@ -91,6 +87,8 @@ function shuffleDeck() {
 }
 
 function deal() {
+    if (hasBeenDealt) return;
+
     shuffleDeck();
     for (let i = 7; i > 0; i--) {
         let toBeRemoved = deckArr.splice(-i, i);
@@ -98,12 +96,12 @@ function deal() {
             activePlaceArr[j].push(toBeRemoved[j]);
         }
     }
-    activePlaceArr.reverse();
-    populateActivePlace();
-    initiateDeck();
+    activePlaceArr.reverse(); // Because Aplace[0] needs to have 1 card but PlaceArr[0] has 7 cards
+    populateActivePlaces();
+    hasBeenDealt = true;
 }
 
-function populateActivePlace() {
+function populateActivePlaces() {
     let activePlaceArrCopy = activePlaceArr;
     for (let i = 0; i < activePlaceArrCopy.length; i++) {
         for (let j = 0; j < activePlaceArrCopy[i].length; j++) {
@@ -116,15 +114,19 @@ function populateActivePlace() {
             Aplaces[i].append(dealtCard);
             topDisplacement = 20 * (Aplaces[i].children.length - 1);
             dealtCard.style.top = `${topDisplacement}px`;
-
-            for (card of cards) { // Capture the ID of the dragged card
-                card.addEventListener("dragstart", (e) => {
-                    e.dataTransfer.setData("text/plain", e.target.id);
-                })
-            }
         }
     }
 
+    for (card of cards) { // Capture the ID of the dragged card
+    card.addEventListener("dragstart", (e) => {
+        e.dataTransfer.setData("text/plain", e.target.id);
+        e.target.style.border = "1px solid red";
+    })
+
+    card.addEventListener("click", (e) => {
+        
+    } )
+}
 }
 
 
@@ -132,7 +134,64 @@ deckPile.addEventListener("click", () => {
     deal();
 })
 
-function move() {
+function move(cardID, target) {
+    let draggedCard = document.getElementById(cardID);
+    let cardRank = idToRank(cardID);
+    let cardSuit = idToSuit(cardID)
+    let targetRank = (target.classList.contains("card")) ? idToRank(target.parentElement.lastChild.id) : null;
+    let targetSuit = (target.classList.contains("card")) ? idToSuit(target.parentElement.lastChild.id) : null;
+
+    console.log("Card Id: " + cardID);
+
+    if (target.classList.contains("foundation-place") || target.parentElement.classList.contains("foundation-place")) {
+        console.log("Dropping on foundation.")
+        if (target.classList.contains("place") && target.children.length === 0) { // when the foundation is empty
+            if (cardRank === 1) {
+                target.append(draggedCard);
+                draggedCard.style.top = "0px";
+            }
+        }
+
+        else if (target.classList.contains("card")) { // when there are other cards in the foundation
+            console.log("there is a card here");
+            if (cardRank === targetRank+1 && cardSuit === targetSuit) {
+                target.parentElement.append(draggedCard);
+                draggedCard.style.top = "0px";
+            }
+        }
+    } 
+    else {
+        if (cardRank === targetRank - 1 || !targetRank) {
+            let topDisplacement = 0;
+            if (target.classList.contains("place")) {
+                target.append(draggedCard);
+                topDisplacement = 0;
+            } else if (target.classList.contains("card")) {
+                target.parentElement.append(draggedCard);
+                topDisplacement = 20 * (target.parentElement.children.length - 1);
+                console.log(topDisplacement);
+                draggedCard.style.top = `${topDisplacement}px`;
+            }
+            draggedCard.style.top = `${topDisplacement}px`;
+    }
+    }
+
 
 }
 
+function idToRank(id) {
+    let numRank;
+    if (!parseInt(id)) {
+        switch (true) {
+            case /A/.test(id): numRank = 1; break;
+            case /J/.test(id): numRank = 11; break;
+            case /Q/.test(id): numRank = 12; break;
+            case /K/.test(id): numRank = 13; break;
+        }
+    } else { numRank = parseInt(id); }
+    return numRank;
+}
+
+function idToSuit(id) {
+    return id[id.length - 1];
+}
